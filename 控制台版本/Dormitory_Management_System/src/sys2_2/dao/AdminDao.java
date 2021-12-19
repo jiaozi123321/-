019e2f43785208a8aa9bda3ecc7f2424e6fbfdaf
HJF <1927817543@qq.com>
2021-12-19 15:58:13 +0800

    .

--- 控制台版本/Dormitory_Management_System/src/sys2_2/dao/AdminDao.java
+++
@@ -1,8 +0,0 @@
-package sys2_2.dao;
-
-import sys2_2.model.Admin;
-
-public interface AdminDao {
-	public int saveAdmin(Admin admin);
-	public Admin getAdmin(String name);
-}
--- 控制台版本/Dormitory_Management_System/src/sys2_2/service/AdminService.java
+++
@@ -1,8 +0,0 @@
-package sys2_2.service;
-
-import sys2_2.model.Admin;
-
-public interface AdminService {
-	public int saveAdmin(Admin admin);
-	public Admin getAdmin(String name);
-}
